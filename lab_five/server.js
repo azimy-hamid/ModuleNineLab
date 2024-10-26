a713aae0ff17231b39b7c0ce7704a34d45d330c6
azimy-hamid <hamidazimy875@gmail.com>
2024-10-26 10:05:13 +0800

    Started working on the simple chat app, but there is a bug where the receiver can't see the msg

---
+++ lab_five/server.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+const PORT = process.env.PORT || 3000;
+
+let users = {};
+
+app.use(express.static("public"));
+
+io.on("connection", (socket) => {
+  console.log("A user connected:", socket.id);
+
+  socket.on("setNickname", (nickname) => {
+    users[socket.id] = nickname;
+    socket.broadcast.emit("userConnected", nickname);
+    io.emit("updateUserList", Object.values(users));
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+    const nickname = users[socket.id];
+    delete users[socket.id];
+    socket.broadcast.emit("userDisconnected", nickname);
+    io.emit("updateUserList", Object.values(users));
+  });
+
+  socket.on("chatMessage", (msg) => {
+    socket.broadcast.emit("chatMessage", {
+      msg,
+      nickname: users[socket.id],
+    });
+  });
+
+  socket.on("typing", () => {
+    socket.broadcast.emit("typing", users[socket.id]);
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
